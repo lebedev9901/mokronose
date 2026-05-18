dabb6a1da78b92cc69bd4140a21b15ebccc512aa
lebedev9901 <155890060+lebedev9901@users.noreply.github.com>
2026-05-18 17:52:58 +0300

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-        <meta name="yandex-verification" content="93a14333f280d765" />
+        <meta name="yandex-verification" content="93a14333f280d765" >
         <title>{{ config('app.name', 'Мокронос') }}</title>
         
 
